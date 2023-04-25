3ba67101c920d2fb2c462b32c8fe242ff1007f8d
Matei2001 <matei.barba@tss-yonder.com>
2023-04-25 20:21:14 +0300

    finished a simple reading from a file in a queue

--- Bachelor Thesis/Program.cs
+++ Bachelor Thesis/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Bachelor_Thesis.Services;
+
+string filePath = "../../../Files/fruits.txt";
+
+Queue<string> elements = new();
+
+var readingTask = Task.Factory.StartNew(() =>
+{
+    new FileReadingService(filePath).ReadFromFile(elements);
+});
+
+
+var writingTask = Task.Factory.StartNew(() =>
+{
+    while (true)
+    {
+        if (elements.Any())
+        {
+            Console.WriteLine(elements.Dequeue());
+        }
+    }
+});
+
+Task.WaitAll(readingTask, writingTask);
---
+++ Bachelor Thesis/Services/FileReadingService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bachelor_Thesis.Services
+{
+    public class FileReadingService
+    {
+        private string _filePath;
+        private int _timeOffSet;
+
+        public FileReadingService(string path, int timeOffSet = 1000)
+        {
+            _filePath = path;
+            _timeOffSet = timeOffSet;
+        }
+
+        public void ReadFromFile(Queue<string> elements)
+        {
+            using(StreamReader reader = new StreamReader(_filePath))
+            {
+                string? line = string.Empty;
+                while (true)
+                {
+                    line = reader.ReadLine();
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        elements.Enqueue(line);
+                    }
+                    Thread.Sleep(_timeOffSet);
+                }
+            }
+        }
+    }
+}
